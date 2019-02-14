371400e833dfa55d917ce02e3a0e68680cd4b4d6
harruup <harrydhillon199@gmail.com>
2019-02-13 19:06:27 -0500

    1

--- app/src/main/java/com/example/kryptonote/KryptoNoteActivity.java
+++ app/src/main/java/com/example/kryptonote/KryptoNoteActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 
 public class KryptoNoteActivity extends AppCompatActivity {
@@ -48,13 +53,49 @@ public class KryptoNoteActivity extends AppCompatActivity {
 
     public void onSave(View v) {
 
-    }
+        try
+        {
+
+            String name = ((EditText) findViewById(R.id.file)).getText().toString();
+            File dir = this.getFilesDir();
+            File file = new File(dir, name);
+            FileWriter fw = new FileWriter(file);
+            fw.write(((EditText) findViewById(R.id.data)).getText().toString());
+            fw.close();
+            Toast.makeText(this, "Note Saved.", Toast.LENGTH_LONG);
+
+        }
+        catch (Exception e) {
+
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+        }
+        }
+
+
 
     public void onLoad(View v) {
 
+        try {
+            String name = ((EditText) findViewById(R.id.file)).getText().toString();
+            File dir = this.getFilesDir();
+            File file = new File(dir, name);
+            FileReader fr = new FileReader(file);
+            String show ="";
+            for (int c = fr.read(); c != -1; c = fr.read())
+            {
+                show += (char) c;
+            }
+            fr.close();
+            ((EditText) findViewById(R.id.data)).setText(show);
+        }
+        catch (Exception e) {
+
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+
+        }
     }
 
-    }
+}
 
 
 
